e68fcd4b9f4d2bf55e9c3b53ccafa5acd2685de3
amosov-f <amosov-f@yandex-team.ru>
2015-07-12 07:54:04 +0300

    Поднять бету для тестинга #3

--- run-server.sh
+++ run-server.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
 # ssh hypernavi@hypernavi.cloudapp.net
-# cd /hol/arkanavt/hypernavi
-# sudo sh run-server.sh
+# sudo sh /hol/arkanavt/hypernavi/run-server.sh
 
 git pull
 mvn3 clean install -Dandroid.sdk.path=/opt/android-sdk-linux
 fuser -KILL -k -n tcp 80
-nohup java -jar server/target/hypernavi-server-jar-with-dependencies.jar -port 80 -cfg /common.properties /test.properties &
+exec java -jar server/target/hypernavi-server-jar-with-dependencies.jar -port 80 -cfg /common.properties /test.properties
