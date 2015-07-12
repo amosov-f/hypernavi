67fcda7dd17218219398eb7247126244180934b9
amosov-f <amosov-f@yandex-team.ru>
2015-07-12 07:39:35 +0300

    Поднять бету для тестинга #3

--- run-server.sh
+++ run-server.sh
@@ -3,4 +3,4 @@
 git pull
 mvn3 clean install -Dandroid.sdk.path=/opt/android-sdk-linux
 fuser -KILL -k -n tcp 80
-java -jar server/target/hypernavi-server.jar -port 80 -cfg /common.properties
+java -jar server/target/hypernavi-server-jar-with-dependencies.jar -port 80 -cfg /common.properties
