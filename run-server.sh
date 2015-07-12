aed5e2a2ea9233d78d2551b296a0288678ea99f1
amosov-f <amosov-f@yandex-team.ru>
2015-07-12 08:07:17 +0300

    Поднять бету для тестинга #3

--- run-server.sh
+++ run-server.sh
@@ -7,3 +7,4 @@ git pull
 mvn3 clean install -Dandroid.sdk.path=/opt/android-sdk-linux
 fuser -KILL -k -n tcp 80
 nohup java -jar server/target/hypernavi-server-jar-with-dependencies.jar -port 80 -cfg /common.properties /test.properties 2>> /dev/null >> /dev/null &
+echo "Server started on port 80!"
