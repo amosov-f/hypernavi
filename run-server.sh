73da5a2233784b3194cb2ffd0a4179d4d72af10e
amosov-f <amosov-f@yandex-team.ru>
2015-07-12 08:19:56 +0300

    Поднять бету для тестинга #3

--- run-server.sh
+++ run-server.sh
@@ -5,7 +5,8 @@
 
 git pull
 mvn3 clean install -Dandroid.sdk.path=/opt/android-sdk-linux
-if [ ! -f server/target/hypernavi-server-jar-with-dependencies.jar ]; then
+rc=$?
+if [[ $rc != 0 ]] ; then
     echo "Maven build failed!"
     sl -e
 fi
