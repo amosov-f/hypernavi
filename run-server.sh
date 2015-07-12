7f11c0869af9d38799aae6fb3f810c75c9727c7e
amosov-f <amosov-f@yandex-team.ru>
2015-07-12 08:13:47 +0300

    Поднять бету для тестинга #3

--- run-server.sh
+++ run-server.sh
@@ -5,6 +5,10 @@
 
 git pull
 mvn3 clean install -Dandroid.sdk.path=/opt/android-sdk-linux
+if [ ! -f server/target/hypernavi-server-jar-with-dependencies.jar ]; then
+    echo "Maven build failed!"
+    sl -e
+fi
 fuser -KILL -k -n tcp 80
 nohup java -jar server/target/hypernavi-server-jar-with-dependencies.jar -port 80 -cfg /common.properties /test.properties 2>> /dev/null >> /dev/null &
 echo "Starting server..."
