cd5edbac6973528d25ff2ea06349b2768eb34209
amosov-f <amosov-f@yandex-team.ru>
2015-07-27 12:27:58 +0300

    Logging example added

--- server/src/main/java/ru/hypernavi/server/servlet/GoodsClassificationService.java
+++ server/src/main/java/ru/hypernavi/server/servlet/GoodsClassificationService.java
@@ -1,16 +1,20 @@
 package ru.hypernavi.server.servlet;
 
-import com.google.common.net.MediaType;
-import com.google.inject.Inject;
 import org.jetbrains.annotations.NotNull;
-import ru.hypernavi.core.classify.GoodsClassifier;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+
+
+import com.google.common.net.MediaType;
+import com.google.inject.Inject;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import ru.hypernavi.core.Category;
+import ru.hypernavi.core.classify.GoodsClassifier;
 
 /**
  * User: amosov-f
@@ -18,6 +22,8 @@ import java.nio.charset.StandardCharsets;
  * Time: 0:35
  */
 public final class GoodsClassificationService extends HttpServlet {
+    private static final Log LOG = LogFactory.getLog(GoodsClassificationService.class);
+
     @NotNull
     private final GoodsClassifier classifier;
 
@@ -27,15 +33,20 @@ public final class GoodsClassificationService extends HttpServlet {
     }
 
     @Override
-    protected void doGet(@NotNull final HttpServletRequest req,
-                         @NotNull final HttpServletResponse resp) throws ServletException, IOException
-    {
+    protected void doGet(@NotNull final HttpServletRequest req, @NotNull final HttpServletResponse resp) throws IOException {
         // request reading
         final String text = req.getParameter("text");
 
+        // business logic
+        final Category category = text != null ? classifier.classify(text) : null;
+        if (category != null) {
+            LOG.info("'" + text + "' classified as '" + category.getName() + "'");
+        }
+
         // response writing
+        resp.setStatus(category != null ? HttpServletResponse.SC_OK : HttpServletResponse.SC_BAD_REQUEST);
         resp.setContentType(MediaType.PLAIN_TEXT_UTF_8.type());
         resp.setCharacterEncoding(StandardCharsets.UTF_8.name());
-        resp.getWriter().println(text != null ? classifier.classify(text).getName() : "No text in query!");
+        resp.getWriter().println(category != null ? category.getName() : "No text in query!");
     }
 }
