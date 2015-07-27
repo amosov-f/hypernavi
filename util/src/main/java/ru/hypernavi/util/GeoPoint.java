163a8d5012c818010e1342010d346c2d988f5bad
PolyakovD <polyakovdmi93@gmail.com>
2015-07-27 23:19:47 +0300

    New method of determining distances is implemented.

--- util/src/main/java/ru/hypernavi/util/GeoPoint.java
+++ util/src/main/java/ru/hypernavi/util/GeoPoint.java
@@ -10,7 +10,28 @@ import org.jetbrains.annotations.NotNull;
 public final class GeoPoint {
     private final double latitude;
     private final double longitude;
-    private static final double EARTH_RADIUS = 6350;
+    private static final double EARTH_RADIUS = 6371.0d;
+    private static final double GRAD_TO_RADIAN = 2.0d * Math.PI / 360.0d;
+
+    public static class my3DPoint{
+        private final double x;
+        private final double y;
+        private final double z;
+
+        private my3DPoint(final double currentX, final double currentY, final double currentZ) {
+            x = currentX;
+            y = currentY;
+            z = currentZ;
+        }
+
+        private my3DPoint(final GeoPoint p) {
+            final double latitudeInRadian = p.getLatitude() * GRAD_TO_RADIAN;
+            final double longitudeInRadian = p.getLongitude() *GRAD_TO_RADIAN;
+            x = EARTH_RADIUS * Math.cos(latitudeInRadian) * Math.cos(longitudeInRadian);
+            y = EARTH_RADIUS * Math.cos(latitudeInRadian) * Math.sin(longitudeInRadian);
+            z = EARTH_RADIUS * Math.sin(latitudeInRadian);
+        }
+    }
 
     public GeoPoint(final double latitude, final double longitude) {
         this.latitude = latitude;
@@ -23,11 +44,15 @@ public final class GeoPoint {
     }
 
     public static double distance(final GeoPoint a, final GeoPoint b) {
-        final double lat = a.getLatitude() - b.getLatitude();
-        final double lon = a.getLongitude() - b.getLongitude();
-        final double angleDistance = Math.sqrt(lat * lat + lon * lon);
-        final double gradToRadian = 2 * Math.PI / 360;
-        return EARTH_RADIUS * gradToRadian * angleDistance;
+
+        final my3DPoint pA = new my3DPoint(a);
+        final my3DPoint pB = new my3DPoint(b);
+        double aBDistance = Math.pow(pA.x - pB.x, 2.0d) + Math.pow(pA.y - pB.y, 2.0d) +  Math.pow(pA.z - pB.z, 2.0d);
+        aBDistance = Math.sqrt(aBDistance);
+        final double cosPhi = 1.0d - aBDistance * aBDistance / (2.0d * EARTH_RADIUS * EARTH_RADIUS);
+        final double phi = Math.acos(cosPhi);
+
+        return EARTH_RADIUS * phi;
     }
 
     @NotNull
--- util/src/test/java/GeoPointTest.java
+++ util/src/test/java/GeoPointTest.java
@@ -6,11 +6,11 @@ import ru.hypernavi.util.GeoPoint;
  * Created by Acer on 27.07.2015.
  */
 public final class GeoPointTest {
-    private static final double EPS = 0.1;
+    private static final double EPS = 1.0;
 
     @Test
     public void test() throws Exception {
-        //test(60.0, 30.0, 59.85588055, 30.0, 16.71);
+        test(60.0, 30.0, 59.85588055, 30.0, 16.71);
         test(60.0, 30.0, 60.0, 30.6098805, 33.93);
     }
 
