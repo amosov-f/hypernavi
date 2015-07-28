158fd4a2f7ccf3bb8ed03541f5f6df40d13f1c52
Константин <BakharevK>
2015-07-28 15:25:35 +0300

    Protected from invalid responce

--- server/src/main/java/ru/hypernavi/server/servlet/SchemaServlet.java
+++ server/src/main/java/ru/hypernavi/server/servlet/SchemaServlet.java
@@ -1,5 +1,6 @@
 package ru.hypernavi.server.servlet;
 
+import org.apache.commons.io.IOExceptionWithCause;
 import org.apache.commons.io.IOUtils;
 import org.jetbrains.annotations.NotNull;
 import ru.hypernavi.util.GeoPoint;
@@ -10,65 +11,83 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
+import java.util.Map;
 
 /**
  * Created by Константин on 19.07.2015.
  */
-public class SchemaServlet extends HttpServlet {
-    public SchemaServlet() throws IOException {
+public final class SchemaServlet extends HttpServlet {
+    @NotNull
+    private final List<BufferedImage> schemas = new ArrayList<>();
+    @NotNull
+    private final List<GeoPoint> coordinates = new ArrayList<>();
+    private static final double MIN_DISTANCE = 40.0;
+
+    public SchemaServlet() {
         readAllSchemas();
     }
 
-    private void readAllSchemas() throws IOException {
+    private void readAllSchemas() {
         final List<Integer> idHyperNavi = new ArrayList<>();
         final List<Double> latitude = new ArrayList<>();
         final List<Double> longitude = new ArrayList<>();
-        schemas = new ArrayList<>();
-        coordinates = new ArrayList<>();
-
-        final String initFile = IOUtils.toString(getClass().getResourceAsStream("/Okey.txt"));
-        final String[] tokens = initFile.split("[ \n]+");
-        for (int i = 0; i < tokens.length; i += 3) {
-            idHyperNavi.add(Integer.parseInt(tokens[i]));
-            latitude.add(Double.parseDouble(tokens[i + 1]));
-            longitude.add(Double.parseDouble(tokens[i + 2]));
+        try {
+            final String initFile = IOUtils.toString(getClass().getResourceAsStream("/Okey.txt"));
+            final String[] tokens = initFile.split("[ \n]+");
+            for (int i = 0; i < tokens.length; i += 3) {
+                idHyperNavi.add(Integer.parseInt(tokens[i]));
+                latitude.add(Double.parseDouble(tokens[i + 1]));
+                longitude.add(Double.parseDouble(tokens[i + 2]));
+            }
         }
-
-        for (int i = 0; i < idHyperNavi.size(); ++i)
+        catch (IOException e)
         {
-            coordinates.add(new GeoPoint(latitude.get(i), longitude.get(i)));
-            schemas.add(ImageIO.read(getClass().getResourceAsStream("/Okey_" + idHyperNavi.get(i).toString() + ".jpg")));
+
+        }
+        try {
+            for (int i = 0; i < idHyperNavi.size(); ++i)
+            {
+                coordinates.add(new GeoPoint(latitude.get(i), longitude.get(i)));
+                schemas.add(ImageIO.read(getClass().getResourceAsStream("/Okey_" + idHyperNavi.get(i).toString() + ".jpg")));
+            }
+        }
+        catch (Exception e)
+        {
+
         }
     }
 
-    private List<BufferedImage> schemas;
-    private List<GeoPoint> coordinates;
-    private static final double MIN_DISTANCE = 40.0;
     @Override
     protected void doGet(@NotNull final HttpServletRequest request,
                          @NotNull final HttpServletResponse response) throws ServletException, IOException {
-        final Double longitude = Double.parseDouble(request.getParameter("longitude"));
-        final Double latitude = Double.parseDouble(request.getParameter("latitude"));
+        final Map<String, String[]> parameterMap = request.getParameterMap();
+        if (!parameterMap.containsKey("lon") || !parameterMap.containsKey("lat"))
+        {
+            response.sendError(HttpServletResponse.SC_BAD_GATEWAY);
+            return;
+        }
+
+        final Double longitude = Double.parseDouble(request.getParameter("lon"));
+        final Double latitude = Double.parseDouble(request.getParameter("lat"));
         final GeoPoint currentPosition = new GeoPoint(latitude, longitude);
 
         final int bestHypernavi = closestHypernavi(currentPosition);
         if (bestHypernavi == -1 || GeoPoint.distance(coordinates.get(bestHypernavi), currentPosition) > MIN_DISTANCE)
         {
-            response.sendError(404);
+            response.sendError(HttpServletResponse.SC_NOT_FOUND);
         }
         else {
+            response.setStatus(HttpServletResponse.SC_OK);
             response.setContentType("image/jpeg");
             final OutputStream out = response.getOutputStream();
             ImageIO.write(schemas.get(bestHypernavi), "jpg", out);
-            out.close();
         }
     }
+
     private int closestHypernavi(final GeoPoint position) {
         double minDistance = Double.POSITIVE_INFINITY;
         int bestHypernavi = -1;
