d2e590342ce716740fe37e547ec58b24f6e09f41
PolyakovD <polyakovdmi93@gmail.com>
2015-07-28 13:39:37 +0300

    GeoPoint correction

--- util/src/main/java/ru/hypernavi/util/GeoPoint.java
+++ util/src/main/java/ru/hypernavi/util/GeoPoint.java
@@ -8,30 +8,9 @@ import org.jetbrains.annotations.NotNull;
  * Time: 23:33
  */
 public final class GeoPoint {
+    private static final double EARTH_RADIUS = 6371.0d;
     private final double latitude;
     private final double longitude;
-    private static final double EARTH_RADIUS = 6371.0d;
-    private static final double GRAD_TO_RADIAN = 2.0d * Math.PI / 360.0d;
-
-    public static class My3DPoint {
-        private final double x;
-        private final double y;
-        private final double z;
-
-        private My3DPoint(final double currentX, final double currentY, final double currentZ) {
-            x = currentX;
-            y = currentY;
-            z = currentZ;
-        }
-
-        private My3DPoint(final GeoPoint p) {
-            final double latitudeInRadian = p.getLatitude() * GRAD_TO_RADIAN;
-            final double longitudeInRadian = p.getLongitude() *GRAD_TO_RADIAN;
-            x = EARTH_RADIUS * Math.cos(latitudeInRadian) * Math.cos(longitudeInRadian);
-            y = EARTH_RADIUS * Math.cos(latitudeInRadian) * Math.sin(longitudeInRadian);
-            z = EARTH_RADIUS * Math.sin(latitudeInRadian);
-        }
-    }
 
     public GeoPoint(final double latitude, final double longitude) {
         this.latitude = latitude;
@@ -43,16 +22,16 @@ public final class GeoPoint {
         return longitude;
     }
 
-    public static double distance(final GeoPoint a, final GeoPoint b) {
+    public static double distance(@NotNull final GeoPoint a, @NotNull final GeoPoint b) {
+        final double aLatitude = Math.toRadians(a.getLatitude());
+        final double bLatitude = Math.toRadians(b.getLatitude());
 
-        final My3DPoint pA = new My3DPoint(a);
-        final My3DPoint pB = new My3DPoint(b);
-        double aBDistance = Math.pow(pA.x - pB.x, 2.0d) + Math.pow(pA.y - pB.y, 2.0d) +  Math.pow(pA.z - pB.z, 2.0d);
-        aBDistance = Math.sqrt(aBDistance);
-        final double cosPhi = 1.0d - aBDistance * aBDistance / (2.0d * EARTH_RADIUS * EARTH_RADIUS);
-        final double phi = Math.acos(cosPhi);
+        final double aLongitude = Math.toRadians(a.getLongitude());
+        final double bLongitude = Math.toRadians(b.getLongitude());
 
-        return EARTH_RADIUS * phi;
+        final double answer = Math.pow(Math.sin((aLatitude - bLatitude) / 2.0d), 2.0d) + Math.cos(aLatitude) * Math.cos(bLatitude)
+                      * Math.pow(Math.sin((aLongitude - bLongitude) / 2.0d), 2.0d);
+        return 2.0d * EARTH_RADIUS * Math.asin(Math.sqrt(answer));
     }
 
     @NotNull
