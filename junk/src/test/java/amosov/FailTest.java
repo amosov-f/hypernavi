9c44049f30751167658b0caba8a8e0f79f805e44
amosov-f <amosov-f@yandex-team.ru>
2015-08-03 22:13:15 +0300

    Настроить TeamCity #12 email notification test

---
+++ junk/src/test/java/amosov/FailTest.java
@@ -0,0 +1,17 @@
+package amosov;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * User: amosov-f
+ * Date: 03.08.15
+ * Time: 23:10
+ */
+public final class FailTest {
+    @Test
+    public void test() {
+        assertEquals(true, false);
+    }
+}
