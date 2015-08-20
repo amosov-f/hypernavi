b77c3b46eaca97b44797d2fef5fa91ca2da2221b
amosov-f <amosov-f@yandex-team.ru>
2015-08-20 22:07:11 +0300

    Настроить TeamCity #12

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+HOST=hypernavi.net
+FILENAME=hypernavi-dev-`stat -c %Y server/target/hypernavi-server-jar-with-dependencies.jar`.jar
+fuser -KILL -k -n tcp 80
+rm -r ~/hypernavi-dev-*
+mv -v server/target/hypernavi-server-jar-with-dependencies.jar ~/${FILENAME}
+nohup java -jar ~/${FILENAME} -port 80 -cfg /common.properties /test.properties -logcfg /log4j-dev.xml -logdir /root/log 2>> /dev/null >> /dev/null &
+
+LOADED=-1
+ATTEMPTS=10
+while [[ ${LOADED} -ne 0 && ${ATTEMPTS} -gt 0 ]]; do
+	sleep 30
+	nc -z -w 1 ${HOST} 80
+	LOADED=$?
+	ATTEMPTS=$(( ATTEMPTS-1 ))
+done
+
+if [[ ${LOADED} -ne 0 ]]; then
+	echo "Server is not responding!"
+	echo "##teamcity[buildStatus status='FAILED' text='Failed do deploy on $HOST']"
+	exit 1
+else
+	echo "Server is started"
+	echo "##teamcity[buildStatus status='SUCCESS' text='Deployed on $HOST']"
+fi
