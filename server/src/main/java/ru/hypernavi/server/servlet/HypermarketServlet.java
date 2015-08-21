1ba808c6a8ba173ea68419d116bbe5ed93b5d80c
Константин <bakharev.k@gmail.com>
2015-08-21 18:38:35 +0300

    HTTP server sevlet

---
+++ server/src/main/java/ru/hypernavi/server/servlet/HypermarketServlet.java
@@ -0,0 +1,24 @@
+package ru.hypernavi.server.servlet;
+
+import org.jetbrains.annotations.NotNull;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import org.apache.commons.io.IOUtils;
+
+/**
+ * Created by Константин on 21.08.2015.
+ */
+@WebServlet(name = "add hyper", value = "/hyper")
+public class HypermarketServlet extends AbstractHttpService {
+
+    @Override
+    public void process(@NotNull final HttpServletRequest req, @NotNull final HttpServletResponse resp) throws IOException {
+        final java.io.OutputStream out = resp.getOutputStream();
+        resp.setContentType("text/html");
+        out.write(IOUtils.toString(HypermarketServlet.class.getResourceAsStream("/hypermarket.html")).getBytes());
+    }
+}
---
+++ server/src/main/java/ru/hypernavi/server/servlet/RegisterHyperServlet.java
@@ -0,0 +1,17 @@
+package ru.hypernavi.server.servlet;
+
+import org.jetbrains.annotations.NotNull;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Константин on 21.08.2015.
+ */
+public class RegisterHyperServlet extends AbstractHttpService {
+    @Override
+    public void process(@NotNull final HttpServletRequest req, @NotNull final HttpServletResponse resp) throws IOException {
+
+    }
+}
