3de737e69ec869a08ec56dc5e5d4cc3e054e6ae0
amosov-f <amosov-f@yandex-team.ru>
2015-08-24 23:07:07 +0300

    Выгружать данные из папки с проектом в указанную директорию во время сборки #33

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
 HOST=hypernavi.net
+
+# moving jar
 FILENAME=hypernavi-dev-`stat -c %Y server/target/hypernavi-server-jar-with-dependencies.jar`.jar
 fuser -KILL -k -n tcp 80
 rm -r ~/hypernavi-dev-*
 mv -v server/target/hypernavi-server-jar-with-dependencies.jar ~/${FILENAME}
+
+# moving data
+mv -v data /root/data
+
+# starting server
 nohup java -jar ~/${FILENAME} -port 80 -cfg /common.properties /dev.properties -logcfg /log4j-dev.xml -logdir /root/log 2>> /dev/null >> /dev/null &
 
+# checking server start
 LOADED=-1
 ATTEMPTS=3
 while [ ${LOADED} -ne 0 ] && [ ${ATTEMPTS} -gt 0 ]; do
