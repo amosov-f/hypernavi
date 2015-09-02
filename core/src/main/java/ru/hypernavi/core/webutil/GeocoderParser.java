003acc4287a6226698f857bea5e025eaf7301a52
Константин <bakharev.k@gmail.com>
2015-09-02 14:37:42 +0300

    Автозаполнение координат гипермаркета по введенному его адресу в админке #25 Запрос адреса на geocoder

---
+++ core/src/main/java/ru/hypernavi/core/webutil/GeocoderParser.java
@@ -0,0 +1,66 @@
+package ru.hypernavi.core.webutil;
+
+import com.sun.istack.internal.Nullable;
+import org.json.JSONException;
+import org.json.JSONObject;
+import ru.hypernavi.util.GeoPoint;
+
+/**
+ * Created by Константин on 17.08.2015.
+ */
+
+public class GeocoderParser {
+    public GeocoderParser() {
+    }
+
+    @Nullable
+    private JSONObject response = null;
+
+    public void setResponce(final JSONObject response) {
+        this.response = response;
+    }
+
+    @Nullable
+    public String getAddress() {
+        String address;
+        try {
+            String location = "";
+            if (response != null) {
+                location = response.getJSONObject("response")
+                        .getJSONObject("GeoObjectCollection")
+                        .getJSONArray("featureMember")
+                        .getJSONObject(0)
+                        .getJSONObject("GeoObject")
+                        .getJSONObject("metaDataProperty")
+                        .getJSONObject("GeocoderMetaData")
+                        .getString("text");
+            }
+            address = location;
+        } catch (JSONException ignored) {
+            address = null;
+        }
+        return address;
+    }
+
+    @Nullable
+    public GeoPoint getLocation() {
+        final GeoPoint position;
+        try {
+            String location = "";
+            if (response != null) {
+                location = response.getJSONObject("response")
+                        .getJSONObject("GeoObjectCollection")
+                        .getJSONArray("featureMember")
+                        .getJSONObject(0)
+                        .getJSONObject("GeoObject")
+                        .getJSONObject("Point")
+                        .getString("pos");
+            }
+            position = GeoPoint.parseString(location);
+        } catch (JSONException ignored) {
+            return null;
+        }
+        return position;
+    }
+}
+
