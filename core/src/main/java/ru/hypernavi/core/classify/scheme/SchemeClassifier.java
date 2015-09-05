078a7cfc20e96f1e55670dadc5637c37122778c6
amosov-f <amosov-f@yandex-team.ru>
2015-09-05 18:32:47 +0300

    small fix

--- core/src/main/java/ru/hypernavi/core/classify/scheme/SchemeClassifier.java
+++ core/src/main/java/ru/hypernavi/core/classify/scheme/SchemeClassifier.java
@@ -2,10 +2,7 @@ package ru.hypernavi.core.classify.scheme;
 
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Arrays;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.Random;
+import java.util.*;
 
 
 import org.apache.commons.io.IOUtils;
@@ -16,9 +13,9 @@ import ru.hypernavi.commons.Chain;
 import ru.hypernavi.core.classify.scheme.answer.ChainAnswer;
 import ru.hypernavi.core.classify.scheme.feature.AuchanHostnameFeature;
 import ru.hypernavi.core.classify.scheme.feature.OkeyHistogramFeature;
-import ru.hypernavi.core.classify.scheme.feature.OkeyHostnameFeature;
 import ru.hypernavi.ml.classifier.BinaryClassifier;
 import ru.hypernavi.ml.classifier.WekaClassifier;
+import ru.hypernavi.ml.factor.Factor;
 import ru.hypernavi.util.EnumUtils;
 import ru.hypernavi.util.MoreIOUtils;
 import weka.classifiers.Evaluation;
@@ -31,8 +28,13 @@ import weka.core.Instances;
 public final class SchemeClassifier extends WekaClassifier<Picture> implements BinaryClassifier<Picture> {
     private static final Log LOG = LogFactory.getLog(SchemeClassifier.class);
 
+    private static final List<? extends Factor<Picture>> FEATURES = Arrays.asList(
+            new OkeyHistogramFeature(),
+            new AuchanHostnameFeature()
+    );
+
     public SchemeClassifier(@NotNull final Picture... dataset) {
-        super(new SMO(), Arrays.asList(new OkeyHostnameFeature(), new OkeyHistogramFeature(), new AuchanHostnameFeature()), new ChainAnswer(), SchemeClassifier::toString, dataset);
+        super(new SMO(), FEATURES, new ChainAnswer(), SchemeClassifier::toString, dataset);
     }
 
     @Override
@@ -55,7 +57,7 @@ public final class SchemeClassifier extends WekaClassifier<Picture> implements B
         final SchemeClassifier classifier = new SchemeClassifier(pictures);
         final Instances instances = classifier.getInstances();
         final Evaluation evaluation = new Evaluation(instances);
-        evaluation.crossValidateModel(classifier.getWekaClassifier(), instances, 5, new Random(0));
+        evaluation.crossValidateModel(classifier.getWekaClassifier(), instances, instances.size(), new Random(0));
         final String tpUrl = "http://www.okmarket.ru/media/gallery/2012-07-27/plan_zala_big.jpg";
         final String tnUrl = "http://cs5134.vk.me/v5134803/12d4/h9q-y0sO6Ko.jpg";
         final Picture tpPicture = Objects.requireNonNull(Picture.download(tpUrl));
