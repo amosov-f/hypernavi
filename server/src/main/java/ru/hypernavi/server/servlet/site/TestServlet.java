fcc0f3f08b8d7497c7a9e145600bfc6de16a838e
Константин <bakharev.k@gmail.com>
2015-09-24 19:03:13 +0300

    Тест русского языка на сервере

---
+++ server/src/main/java/ru/hypernavi/server/servlet/site/TestServlet.java
@@ -0,0 +1,35 @@
+package ru.hypernavi.server.servlet.site;
+
+import org.jetbrains.annotations.NotNull;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.logging.Logger;
+
+
+import com.google.common.net.MediaType;
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpStatus;
+import ru.hypernavi.server.servlet.AbstractHttpService;
+
+/**
+ * Created by Константин on 23.09.2015.
+ */
+@WebServlet(name = "Test", value = "/testRussian")
+public class TestServlet extends AbstractHttpService {
+    private static final Logger LOG = Logger.getLogger(TestServlet.class.getName());
+
+    @Override
+    public final void process(@NotNull final HttpServletRequest req, @NotNull final HttpServletResponse resp) throws IOException {
+        resp.setStatus(HttpStatus.SC_OK);
+        resp.setContentType(MediaType.ANY_TEXT_TYPE.type());
+        resp.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        final String example = "Тестовый текст.";
+        LOG.info(example);
+        IOUtils.write(example.getBytes(), resp.getOutputStream());
+    }
+
+}
