8d3feb11bdf0ec4b62a4e282ede9d455e85bc09b
amosov-f <amosov-f@yandex-team.ru>
2015-10-10 13:26:55 +0300

    Поднять nginx на hypernavi.net:80 #62 morda path fixed

--- server/src/main/java/ru/hypernavi/server/servlet/admin/MordaHttpService.java
+++ server/src/main/java/ru/hypernavi/server/servlet/admin/MordaHttpService.java
@@ -12,7 +12,7 @@ import ru.hypernavi.server.servlet.HtmlPageHttpService;
 /**
  * Created by amosov-f on 25.08.15.
  */
-@WebServlet(name = "morda", value = "/")
+@WebServlet(name = "morda", value = "")
 public final class MordaHttpService extends HtmlPageHttpService {
     @Inject
     public MordaHttpService(@NotNull final Configuration templatesConfig) {
