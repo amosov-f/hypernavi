da0948ddd3acbf3c29f9f1f937e7e1fe25a81284
amosov-f <amosov-f@yandex-team.ru>
2016-03-05 23:38:50 +0300

    katya's site

---
+++ server/src/main/java/ru/hypernavi/server/servlet/example/ExampleService.java
@@ -0,0 +1,31 @@
+package ru.hypernavi.server.servlet.example;
+
+import org.jetbrains.annotations.NotNull;
+
+
+import javax.servlet.annotation.WebServlet;
+
+
+import com.google.inject.Inject;
+import ru.hypernavi.core.session.RequestReader;
+import ru.hypernavi.core.session.Session;
+import ru.hypernavi.server.servlet.HtmlPageHttpService;
+
+/**
+ * User: amosov-f
+ * Date: 05.03.16
+ * Time: 23:31
+ */
+@WebServlet(name = "example site", value = "/example")
+public final class ExampleService extends HtmlPageHttpService {
+    @Inject
+    public ExampleService(@NotNull final RequestReader.Factory<?> init) {
+        super(init);
+    }
+
+    @NotNull
+    @Override
+    public String getPathInBundle(@NotNull final Session session) {
+        return "example/example.ftl";
+    }
+}
---
+++ server/src/main/resources/web/pages/example/example.ftl
@@ -0,0 +1,19 @@
+<html>
+<head>
+    <title>TODO supply a title</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width">
+
+</head>
+<body>
+<ul>
+    <li><a href="#">Австралия</a></li>
+    <li><a href="#">Бразилия</a></li>
+    <li><a href="#">Россия</a></li>
+    <li><a href="#">Япония</a></li>
+</ul>
+
+
+
+</body>
+</html>
