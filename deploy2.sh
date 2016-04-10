6608f2d2579a7ab45c168be0bdd7aed8239b3516
amosov-f <amosov-f@yandex-team.ru>
2016-04-10 22:14:19 +0300

    fix

--- deploy2.sh
+++ deploy2.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
+IMAGES=$(docker ps -a -q --filter ancestor=amosov/hypernavi --format="{{.ID}}")
 docker build -t amosov/hypernavi .
 docker tag $(docker images -q amosov/hypernavi) amosov/hypernavi
 docker push amosov/hypernavi
-docker rm $(docker stop $(docker ps -a -q --filter ancestor=amosov/hypernavi --format="{{.ID}}"))
+docker rm $(docker stop $IMAGES)
 docker run -p 80:80 -d amosov/hypernavi
