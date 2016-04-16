4dd7d91eb7b7cd226c8ee0b87680f932f961f5e7
amosov-f <amosov-f@yandex-team.ru>
2016-04-16 21:03:54 +0300

    configure user

--- mongodump.sh
+++ mongodump.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
 
+GIT_COMMITER_NAME="zomb-amosov-f"
+GIT_COMMITER_EMAIL="amosov.fed@yandex.ru"
+
+git_configure_user() {
+  DIR=$1
+  git -C ${DIR} config user.name "${GIT_COMMITER_NAME}"
+  git -C ${DIR} config user.email "${GIT_COMMITER_EMAIL}"
+}
+
+git_configure_user /root/hypernavi
 cd /root/hypernavi-data
 git commit -am "data updated" && git push origin master
