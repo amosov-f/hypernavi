3f4d380acc766d67bef9e5f7e560fafc4a101db3
amosov-f <amosov-f@yandex-team.ru>
2016-04-16 20:50:15 +0300

    dump script

--- deploy2.sh
+++ deploy2.sh
@@ -9,3 +9,6 @@ docker tag $(docker images -q amosov/hypernavi) amosov/hypernavi
 docker push amosov/hypernavi
 docker rm $(docker stop $IMAGES)
 docker run -p 80:80 -d amosov/hypernavi
+
+# mongodb
+# docker run -p 27017:27017 -v /root/hypernavi-data/data/db:/data/db -d mongo
