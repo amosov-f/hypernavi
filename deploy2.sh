9b0bac0aa98bfcb4d47d38a2d9c4f9049ec06f26
amosov-f <amosov-f@yandex-team.ru>
2016-04-17 12:21:20 +0300

    docker run name

--- deploy2.sh
+++ deploy2.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/env bash
 
-# moving data
-rsync -a data /root
-
-IMAGES=$(docker ps -a -q --filter ancestor=amosov/hypernavi --format="{{.ID}}")
 docker build -t amosov/hypernavi .
 docker tag $(docker images -q amosov/hypernavi) amosov/hypernavi
 docker push amosov/hypernavi
-docker rm $(docker stop $IMAGES)
-docker run -p 7340:80 -d amosov/hypernavi
+docker stop hypernavi
+docker rm hypernavi
+docker run --name hypernavi -p 7340:80 -d amosov/hypernavi
 
 # mongodb
-# docker run -p 27017:27017 -v /root/hypernavi-data/data/db:/data/db -d mongo
-# docker run -p 80:80 -v /root/hypernavi-data/nginx.conf:/etc/nginx/nginx.conf:ro -v /root/hypernavi-data/img:/root/data/img -d nginx
+# docker run --name mongo -p 27017:27017 -v /root/hypernavi-data/data/db:/data/db -d mongo
+# docker run --name nginx -p 80:80 -v /root/hypernavi-data/nginx.conf:/etc/nginx/conf.d/default.conf -v /root/hypernavi-data/data/img:/usr/share/nginx/img -d nginx
