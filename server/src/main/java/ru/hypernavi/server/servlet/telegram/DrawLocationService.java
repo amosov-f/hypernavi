48e76a37d7b164bf8b04c961a9ab88d9e6068c8c
amosov-f <amosov-f@yandex-team.ru>
2016-12-17 19:16:23 +0300

    draw location service

---
+++ server/src/main/java/ru/hypernavi/server/servlet/telegram/DrawLocationService.java
@@ -0,0 +1,30 @@
+package ru.hypernavi.server.servlet.telegram;
+
+import org.jetbrains.annotations.NotNull;
+import ru.hypernavi.core.session.Property;
+import ru.hypernavi.core.session.Session;
+import ru.hypernavi.core.webutil.ImageEditor;
+import ru.hypernavi.server.servlet.AbstractHttpService;
+import ru.hypernavi.util.awt.ImageUtils;
+
+import javax.imageio.ImageIO;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+/**
+ * Created by amosov-f on 17.12.16
+ */
+@WebServlet(name = "draw location", value = "/draw/location")
+public final class DrawLocationService extends AbstractHttpService {
+  @Override
+  public void service(@NotNull final Session session, @NotNull final HttpServletResponse resp) throws IOException {
+    final Point location = session.demand(Property.MAP_POINT);
+    final String imageLink = session.demand(Property.LINK);
+    final BufferedImage image = ImageUtils.download(imageLink);
+    final BufferedImage locatedImage = ImageEditor.INSTANCE.drawLocation(image, location);
+    ImageIO.write(locatedImage, ImageUtils.format(locatedImage, imageLink), resp.getOutputStream());
+  }
+}
