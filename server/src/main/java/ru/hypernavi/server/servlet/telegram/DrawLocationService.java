c2d9afc3fd0feca9b447459cc451b60fc9a500b9
amosov-f <amosov-f@yandex-team.ru>
2016-12-18 15:08:57 +0300

    more logging

--- server/src/main/java/ru/hypernavi/server/servlet/telegram/DrawLocationService.java
+++ server/src/main/java/ru/hypernavi/server/servlet/telegram/DrawLocationService.java
@@ -1,5 +1,7 @@
 package ru.hypernavi.server.servlet.telegram;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.jetbrains.annotations.NotNull;
 import ru.hypernavi.core.session.Property;
 import ru.hypernavi.core.session.Session;
@@ -19,6 +21,8 @@ import java.io.IOException;
  */
 @WebServlet(name = "draw location", value = "/draw/location")
 public final class DrawLocationService extends AbstractHttpService {
+  private static final Log LOG = LogFactory.getLog(DrawLocationService.class);
+
   @Override
   public void service(@NotNull final Session session, @NotNull final HttpServletResponse resp) throws IOException {
     resp.setStatus(HttpServletResponse.SC_OK);
@@ -27,6 +31,8 @@ public final class DrawLocationService extends AbstractHttpService {
     final BufferedImage image = ImageUtils.download(imageLink);
     final BufferedImage locatedImage = ImageEditor.INSTANCE.drawLocation(image, location);
     resp.setContentType("image/jpeg");
+    final long start = System.currentTimeMillis();
     ImageIO.write(locatedImage, ImageUtils.format(locatedImage, imageLink), resp.getOutputStream());
+    LOG.debug("Writing image to response finished in " + (System.currentTimeMillis() - start) + " ms");
   }
 }
