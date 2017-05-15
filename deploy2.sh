e6a9af4ee3424574817402bcb8bdca12859b62ef
amosov-f <amosov-f@yandex-team.ru>
2017-05-15 23:58:03 +0300

    telegram token upload doc

--- deploy2.sh
+++ deploy2.sh
@@ -29,3 +29,4 @@ docker run --name hypernavi -p 7340:80 -v /root/log:/root/log -v /root/hypernavi
 
 # https
 # sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/nginx.key -out /etc/nginx/ssl/nginx.crt
+# curl -F "url=https://hypernavi.net/telegram/TOKEN" -F "certificate=@/etc/nginx/ssl/nginx.crt" https://api.telegram.org/botTOKEN/setWebhook
